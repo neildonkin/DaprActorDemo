826c33fbdbf3694cfacad4b3316da6f28d8207a0
Neil Donkin <neildonkin@outlook.com>
2020-10-06 08:31:16 +0100

    Added the Dapr.Actors nuget reference

---
+++ DaprActorDemo.Interfaces/Interfaces.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DaprActorDemo.Interfaces
+{
+    public interface IStoreActor
+    {
+        Task<string> SetDataAsync(StoreInfo data);
+        Task<StoreInfo> GetDataAsync();
+        Task RegisterReminder();
+        Task UnregisterReminder();
+        Task RegisterTimer();
+        Task UnregisterTimer();
+    }
+
+    public class StoreInfo
+    {
+        public string Id { get; set; }
+        
+        public int Latitude { get; set; }
+
+        public int Longitude { get; set; }
+
+        public string Address { get; set; }
+
+        public string Telephone { get; set; }
+    }
+}
